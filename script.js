b663674d9900da6f762cb1c0038d73a4d86c1c03
edisa-nushi <edisa@skooli.com>
2022-03-01 02:56:30 +0100

    added more functionalities to the calculator and fixed some bugs

--- script.js
+++ script.js
@@ -3,32 +3,41 @@ const numberButton = document.querySelectorAll('.number-button');
 const operatorButton = document.querySelectorAll('.operator-button');
 const actionButton = document.querySelectorAll('.action-button');
 
-let operator;
-// let firstNumber = 0;
-let secondNumber = 0;
+let operator = null;
+let firstNumber = 0;
+let secondNumber = 0; 
 let result = 0;
-let secNumArray = [];
+let output;
+let secNumArray = [];   //stores all the values of the field before clicking on '='
+let buttonClicked = []; //stores the values of the clicked buttons in the order they are clicked
+let lastClicked;          //holds the value of the last clicked button.
 
+
+//writes the numbers in the field as the user clicks on the respective buttons.
 for (let i=0; i<numberButton.length; i++){
     numberButton[i].addEventListener('click', function(){
+        buttonClicked.push(numberButton[i].innerText);
+        lastClicked = buttonClicked[buttonClicked.length-2];
         if (outputField.innerText === '0'){
-            outputField.innerText = numberButton[i].innerText;
+            outputField.innerText = numberButton[i].innerText;   //if there is only a 0 in the output field, it will be replaced with the value of the clicked number button.
         }
-        else if (outputField.innerText === '+' || outputField.innerText === '-' || outputField.innerText === '×' || outputField.innerText === '÷'){
-            outputField.innerText = numberButton[i].innerText;
-        }
-        else {
-            outputField.innerText = outputField.innerText + numberButton[i].innerText;
+        else {  
+            if (lastClicked === '=' || lastClicked === '+' || lastClicked === '-' || lastClicked === '×' || lastClicked === '÷'){
+                outputField.innerText = numberButton[i].innerText; //if before this, the last clicked button was '=' or any of the operators(+,-,x,±), the value on the field will be replaced with the value of this number button.
+            }
+            else {
+                outputField.innerText = outputField.innerText + numberButton[i].innerText;   //in any other case, the value of the clicked number will be added at the end of the string in output field
+            }
         }
     });
 }
 
-
+//assigns values to firstNumber and operator variables
 for (let i=0; i<operatorButton.length; i++){
-    operatorButton[i].addEventListener('click', function(){
-        result = parseInt(outputField.innerText);
+    operatorButton[i].addEventListener('click', function(event){
+        buttonClicked.push(operatorButton[i].innerText);
+        firstNumber = parseInt(outputField.innerText);
         operator = operatorButton[i].innerText;
-        outputField.innerText = operatorButton[i].innerText;
         secNumArray = [];
     });
 }
@@ -36,57 +45,73 @@ for (let i=0; i<operatorButton.length; i++){
 //takes an action based on the action button clicked(for example clearing the field, clearing the latest character entered or calculating the result)
 for (let i=0; i<actionButton.length; i++){
     actionButton[i].addEventListener('click', function(){
+        buttonClicked.push(actionButton[i].innerText);
         switch (actionButton[i].innerText){
             case ('C'):
-                outputField.innerText = '0';
-                secNumArray = [];
-                operator = null;
-                result = 0; 
+                deleteEverything();
                 break;
             case ('←'):
-                let output = outputField.innerText;
-                if (output.length > 1){
-                    output = output.substring(0, output.length-1);
-                    outputField.innerText = output;
-               }
-                else {
-                    outputField.innerText = '0';
-               }
-               break;
+                doBackspace();
+                break;
             case ('='):
-                calculateResult();   
+                calculateResult();
+                break;
         }
     });
 }
 
-//calculates the result based on the operator
-function calculateResult(){
-    secondNumber = parseInt(outputField.innerText);
-    secNumArray.push(secondNumber);
-    let bool = isNaN(secondNumber);
-    if (bool === true){
-        outputField.innerText = '0';
+function deleteEverything() {  //deletes all data
+    outputField.innerText = '0';
+    firstNumber = 0; 
+    secNumArray = [];
+    result = 0;
+    operator = null;
+}
+
+function doBackspace(){  //removes the latest character on the field, and if it is the last character remaining replaces it with 0.
+    output = outputField.innerText;
+    if (output.length > 1){
+        output = output.substring(0, output.length-1);
+        outputField.innerText = output;
     }
     else {
-        switch(operator){
-            case ('+'):
-                console.log (result + " + " + secNumArray[0] + " = " + (result+secNumArray[0]));
-                result = result + secNumArray[0];
-                break;
-            case ('-'):
-                console.log (result + " - " + secNumArray[0] + " = " + (result-secNumArray[0]));
-                result = result - secNumArray[0];
-                break;
-            case ('×'):
-                console.log (result + " × " + secNumArray[0] + " = " + (result*secNumArray[0]));
-                result = result * secNumArray[0]; 
-                break;
-            case ('÷'):
-                console.log (result + " ÷ " + secNumArray[0] + " = " + (result/secNumArray[0]));
-                result = result/ secNumArray[0];
-                break;  
-       }
+        outputField.innerText = '0';
     }
-    outputField.innerText = result.toString();
-    // operator = null;  
+}
+
+function calculateResult(){  //calculates the result based on the operator
+    secondNumber = parseInt(outputField.innerText);
+    lastClicked = buttonClicked[buttonClicked.length-2];
+    if (lastClicked !== '+' && lastClicked !== '-' && lastClicked !== '×' && lastClicked !== '÷'){
+        secNumArray.push(secondNumber);
+        if (operator === null){
+            outputField.innerText = secondNumber.toString(); 
+        }
+        else {
+            switch(operator){
+                case ('+'):
+                    result = firstNumber + secNumArray[0];
+                    console.log (firstNumber + " + " + secNumArray[0] + " = " + result);
+                    break;
+                case ('-'):
+                    result = firstNumber - secNumArray[0];
+                    console.log (firstNumber + " - " + secNumArray[0] + " = " + result);
+                    break;
+                case ('×'):
+                    result = firstNumber * secNumArray[0]; 
+                    console.log (firstNumber + " × " + secNumArray[0] + " = " + result);
+                    break;
+                case ('÷'):
+                    result = firstNumber/ secNumArray[0];
+                    console.log (firstNumber + " ÷ " + secNumArray[0] + " = " + result);
+                    break;  
+            }
+            outputField.innerText = result.toString();
+            firstNumber = result;
+        }
+    }
+    else {  //if the last clicked button(before clicking =) is one of the operators, don't do any calculation
+        outputField.innerText = outputField.innerText;
+        buttonClicked.pop();
+    } 
 }
